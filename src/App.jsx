52b5c38e9ab46d9b6e0f2e0bd5e07430c5aa1f40
George <georgejoj28@gmail.com>
2023-11-21 23:34:23 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import ProjectList from "./ProjectList";
+
+/*
+Client Side Routing
+- URLs
+- REST
+
+
+*/
+
+function App() {
+    const [page, setPage] = useState("/")
+    
+    function getCurrentPage() {
+        switch(page) {
+            case "/":
+                return <Home />
+            case "/about":
+                return <About />
+            case "/projects":
+                return <ProjectList />
+            default:
+                return <h1>404 not found</h1>
+        }
+    }
+    
+    return (
+        <div>
+            <NavBar onChangePage={setPage} />
+            {getCurrentPage()}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div id="home">
+            <h1 style={{ color: "firebrick" }}>
+                George is a Web Developer from Nairobi
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
